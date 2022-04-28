ea2a14fdef568d95bcc9a7c29ca8213f28d95a16
Ali Salehi <alisalehi69@live.com>
2022-04-28 14:24:39 +0300

    lesson 4

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => {
+        return <CategoryItem category={category} key={category.id} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
