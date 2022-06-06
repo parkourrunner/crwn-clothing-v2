393a4a1a2556d173647c51603eeb4e503f9b0b05
Ali Salehi <parkour.runner@gmail.com>
2022-06-06 10:11:45 +0300

    lesson 18

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,6 +1,10 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+// import SHOP_DATA from "../shop-data.js";
 
+import {
+  getCategoriesAndDocuments,
+  addCollectionAndDocuments,
+} from "../utils/firebase/firebase.utils.js";
 export const ProductsContext = createContext({
   products: [],
   id: null,
@@ -10,9 +14,16 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    // addCollectionAndDocuments("categories", SHOP_DATA);
+    getCategoriesMap();
+  }, []);
   const value = { products };
-  //   setProducts(PRODUCTS);
 
   return (
     <ProductsContext.Provider value={value}>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyBaGs7kmcMR2CLB29SQ6A87AZdOnKlmGjc",
   authDomain: "crwn-clothing-db-c8329.firebaseapp.com",
@@ -35,6 +44,36 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querrySnapshot = await getDocs(q);
+  const categoryMap = querrySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
