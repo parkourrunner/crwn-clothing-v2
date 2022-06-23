5ceb5da6a50dbc625b6687495ac1efa1348441ce
Ali Salehi <parkour.runner@gmail.com>
2022-06-23 11:07:50 +0300

    lesson 35

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,18 +4,13 @@ import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categories = await getCategoriesAndDocuments("categories");
-      dispatch(setCategories(categories));
-    };
-    return getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
   return (
     <Routes>
